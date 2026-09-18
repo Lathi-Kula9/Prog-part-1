45b220d871ca66d130e49fa2267e5ab6beb52332
Lathi-Kula9 <kulahoyihlumelo@gmail.com>
2026-09-18 15:29:45 +0200

    Implement All the information in Login.java

---
+++ test/LoginTestTest.java
@@ -0,0 +1,359 @@
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kulah
+ */
+public class LoginTestTest {
+    
+    public LoginTestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of testUserName_CorrectlyFormatted method, of class LoginTest.
+     */
+    @Test
+    public void testTestUserName_CorrectlyFormatted() {
+        System.out.println("testUserName_CorrectlyFormatted");
+        LoginTest instance = new LoginTest();
+        instance.testUserName_CorrectlyFormatted();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testUserName_IncorrectlyFormatted method, of class LoginTest.
+     */
+    @Test
+    public void testTestUserName_IncorrectlyFormatted() {
+        System.out.println("testUserName_IncorrectlyFormatted");
+        LoginTest instance = new LoginTest();
+        instance.testUserName_IncorrectlyFormatted();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testUserName_UnderscoreAtStartStillWorks method, of class LoginTest.
+     */
+    @Test
+    public void testTestUserName_UnderscoreAtStartStillWorks() {
+        System.out.println("testUserName_UnderscoreAtStartStillWorks");
+        LoginTest instance = new LoginTest();
+        instance.testUserName_UnderscoreAtStartStillWorks();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testUserName_HasUnderscoreButTooLong method, of class LoginTest.
+     */
+    @Test
+    public void testTestUserName_HasUnderscoreButTooLong() {
+        System.out.println("testUserName_HasUnderscoreButTooLong");
+        LoginTest instance = new LoginTest();
+        instance.testUserName_HasUnderscoreButTooLong();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPassword_MeetsComplexityRequirements method, of class LoginTest.
+     */
+    @Test
+    public void testTestPassword_MeetsComplexityRequirements() {
+        System.out.println("testPassword_MeetsComplexityRequirements");
+        LoginTest instance = new LoginTest();
+        instance.testPassword_MeetsComplexityRequirements();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPassword_DoesNotMeetComplexityRequirements method, of class LoginTest.
+     */
+    @Test
+    public void testTestPassword_DoesNotMeetComplexityRequirements() {
+        System.out.println("testPassword_DoesNotMeetComplexityRequirements");
+        LoginTest instance = new LoginTest();
+        instance.testPassword_DoesNotMeetComplexityRequirements();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPassword_ExactlyEightCharactersAndValid method, of class LoginTest.
+     */
+    @Test
+    public void testTestPassword_ExactlyEightCharactersAndValid() {
+        System.out.println("testPassword_ExactlyEightCharactersAndValid");
+        LoginTest instance = new LoginTest();
+        instance.testPassword_ExactlyEightCharactersAndValid();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPassword_MissingOnlySpecialCharacter method, of class LoginTest.
+     */
+    @Test
+    public void testTestPassword_MissingOnlySpecialCharacter() {
+        System.out.println("testPassword_MissingOnlySpecialCharacter");
+        LoginTest instance = new LoginTest();
+        instance.testPassword_MissingOnlySpecialCharacter();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testPassword_HasEveryCharacterTypeButTooShort method, of class LoginTest.
+     */
+    @Test
+    public void testTestPassword_HasEveryCharacterTypeButTooShort() {
+        System.out.println("testPassword_HasEveryCharacterTypeButTooShort");
+        LoginTest instance = new LoginTest();
+        instance.testPassword_HasEveryCharacterTypeButTooShort();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testCellPhoneNumber_CorrectlyFormatted method, of class LoginTest.
+     */
+    @Test
+    public void testTestCellPhoneNumber_CorrectlyFormatted() {
+        System.out.println("testCellPhoneNumber_CorrectlyFormatted");
+        LoginTest instance = new LoginTest();
+        instance.testCellPhoneNumber_CorrectlyFormatted();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testCellPhoneNumber_IncorrectlyFormatted method, of class LoginTest.
+     */
+    @Test
+    public void testTestCellPhoneNumber_IncorrectlyFormatted() {
+        System.out.println("testCellPhoneNumber_IncorrectlyFormatted");
+        LoginTest instance = new LoginTest();
+        instance.testCellPhoneNumber_IncorrectlyFormatted();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testCellPhoneNumber_ExactlyTenDigitsAfterCode method, of class LoginTest.
+     */
+    @Test
+    public void testTestCellPhoneNumber_ExactlyTenDigitsAfterCode() {
+        System.out.println("testCellPhoneNumber_ExactlyTenDigitsAfterCode");
+        LoginTest instance = new LoginTest();
+        instance.testCellPhoneNumber_ExactlyTenDigitsAfterCode();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testCellPhoneNumber_ElevenDigitsAfterCodeFails method, of class LoginTest.
+     */
+    @Test
+    public void testTestCellPhoneNumber_ElevenDigitsAfterCodeFails() {
+        System.out.println("testCellPhoneNumber_ElevenDigitsAfterCodeFails");
+        LoginTest instance = new LoginTest();
+        instance.testCellPhoneNumber_ElevenDigitsAfterCodeFails();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testCellPhoneNumber_LettersMixedInFails method, of class LoginTest.
+     */
+    @Test
+    public void testTestCellPhoneNumber_LettersMixedInFails() {
+        System.out.println("testCellPhoneNumber_LettersMixedInFails");
+        LoginTest instance = new LoginTest();
+        instance.testCellPhoneNumber_LettersMixedInFails();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testRegisterUser_AllDetailsValid method, of class LoginTest.
+     */
+    @Test
+    public void testTestRegisterUser_AllDetailsValid() {
+        System.out.println("testRegisterUser_AllDetailsValid");
+        LoginTest instance = new LoginTest();
+        instance.testRegisterUser_AllDetailsValid();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testRegisterUser_FailsOnUsername method, of class LoginTest.
+     */
+    @Test
+    public void testTestRegisterUser_FailsOnUsername() {
+        System.out.println("testRegisterUser_FailsOnUsername");
+        LoginTest instance = new LoginTest();
+        instance.testRegisterUser_FailsOnUsername();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testRegisterUser_FailsOnPassword method, of class LoginTest.
+     */
+    @Test
+    public void testTestRegisterUser_FailsOnPassword() {
+        System.out.println("testRegisterUser_FailsOnPassword");
+        LoginTest instance = new LoginTest();
+        instance.testRegisterUser_FailsOnPassword();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testRegisterUser_FailsOnCellPhoneNumber method, of class LoginTest.
+     */
+    @Test
+    public void testTestRegisterUser_FailsOnCellPhoneNumber() {
+        System.out.println("testRegisterUser_FailsOnCellPhoneNumber");
+        LoginTest instance = new LoginTest();
+        instance.testRegisterUser_FailsOnCellPhoneNumber();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testRegisterUser_DifferentValidDetailsStillSucceed method, of class LoginTest.
+     */
+    @Test
+    public void testTestRegisterUser_DifferentValidDetailsStillSucceed() {
+        System.out.println("testRegisterUser_DifferentValidDetailsStillSucceed");
+        LoginTest instance = new LoginTest();
+        instance.testRegisterUser_DifferentValidDetailsStillSucceed();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testRegisterUser_PasswordCheckedBeforeCellPhoneNumber method, of class LoginTest.
+     */
+    @Test
+    public void testTestRegisterUser_PasswordCheckedBeforeCellPhoneNumber() {
+        System.out.println("testRegisterUser_PasswordCheckedBeforeCellPhoneNumber");
+        LoginTest instance = new LoginTest();
+        instance.testRegisterUser_PasswordCheckedBeforeCellPhoneNumber();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testLoginUser_Successful method, of class LoginTest.
+     */
+    @Test
+    public void testTestLoginUser_Successful() {
+        System.out.println("testLoginUser_Successful");
+        LoginTest instance = new LoginTest();
+        instance.testLoginUser_Successful();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testLoginUser_Failed method, of class LoginTest.
+     */
+    @Test
+    public void testTestLoginUser_Failed() {
+        System.out.println("testLoginUser_Failed");
+        LoginTest instance = new LoginTest();
+        instance.testLoginUser_Failed();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testLoginUser_WrongCaseUsernameFails method, of class LoginTest.
+     */
+    @Test
+    public void testTestLoginUser_WrongCaseUsernameFails() {
+        System.out.println("testLoginUser_WrongCaseUsernameFails");
+        LoginTest instance = new LoginTest();
+        instance.testLoginUser_WrongCaseUsernameFails();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testLoginUser_EmptyCredentialsFail method, of class LoginTest.
+     */
+    @Test
+    public void testTestLoginUser_EmptyCredentialsFail() {
+        System.out.println("testLoginUser_EmptyCredentialsFail");
+        LoginTest instance = new LoginTest();
+        instance.testLoginUser_EmptyCredentialsFail();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testReturnLoginStatus_Successful method, of class LoginTest.
+     */
+    @Test
+    public void testTestReturnLoginStatus_Successful() {
+        System.out.println("testReturnLoginStatus_Successful");
+        LoginTest instance = new LoginTest();
+        instance.testReturnLoginStatus_Successful();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testReturnLoginStatus_Failed method, of class LoginTest.
+     */
+    @Test
+    public void testTestReturnLoginStatus_Failed() {
+        System.out.println("testReturnLoginStatus_Failed");
+        LoginTest instance = new LoginTest();
+        instance.testReturnLoginStatus_Failed();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testReturnLoginStatus_SuccessfulWithDifferentUser method, of class LoginTest.
+     */
+    @Test
+    public void testTestReturnLoginStatus_SuccessfulWithDifferentUser() {
+        System.out.println("testReturnLoginStatus_SuccessfulWithDifferentUser");
+        LoginTest instance = new LoginTest();
+        instance.testReturnLoginStatus_SuccessfulWithDifferentUser();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
